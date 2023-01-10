fa0f32432fb6b0a549b6abd205564adb5559b7eb
Morozzzuk <115870040+Morozzzuk@users.noreply.github.com>
2023-01-10 19:07:28 +0000

    Deploying to gh-pages from @ ArtemDruz/fson-74-himars@d1f7e85296e3f3ed343228d09df0bfbf1eebbb69 🚀

---
+++ index.02957ef7.js
@@ -0,0 +1,2 @@
+$(".slider").slick({dots:!0,arrows:!1,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0});
+//# sourceMappingURL=index.02957ef7.js.map
---
+++ index.b6a30c44.js
@@ -0,0 +1,2 @@
+$(".slider").slick({dots:!0,arrows:!1,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0});
+//# sourceMappingURL=index.b6a30c44.js.map
